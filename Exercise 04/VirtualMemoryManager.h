227abb97191275e3bb53695041a4f520ec0fa09e
Noam Shabat <92057540+Noamshabat1@users.noreply.github.com>
2024-07-17 12:30:35 +0300

    Update VirtualMemoryManager.h

--- Exercise 04/VirtualMemoryManager.h
+++ Exercise 04/VirtualMemoryManager.h
@@ -22,35 +22,37 @@ public:
 private:
     word_t manageMemory(uint64_t virtualAddress, int depth);
 
-    word_t findEmptyFrame(const word_t ancestors[TABLES_DEPTH]);
+    word_t findEmptyFrame(const word_t parents[TABLES_DEPTH]);
 
     word_t findUnusedFrame();
 
-    word_t evictFrameWithMaxCyclicalDistance(word_t pageSwappedIn, const word_t ancestors[TABLES_DEPTH]);
+    word_t evictFrameWithMaxCyclicalDistance(word_t pageSwappedIn, const word_t parents[TABLES_DEPTH]);
 
-    word_t findFrameToUse(word_t pageSwappedIn, const word_t ancestors[TABLES_DEPTH]);
+    word_t findFrameToUse(word_t pageSwappedIn, const word_t parents[TABLES_DEPTH]);
 
-    bool isAncestor(word_t frame, const word_t ancestors[TABLES_DEPTH]);
+    static bool didComeFromTheSamePath(word_t frame, const word_t parents[4]);
 
-    void resetFrame(word_t frame);
+    static void resetFrame(word_t frame);
 
     void removeParentLink(word_t frame, int depth, word_t targetFrame);
 
-    bool isitALeaf(word_t frame, int depth, word_t targetFrame);
+    bool isItALeaf(word_t frame, int depth, word_t targetFrame);
 
-    word_t getFrameByPage(word_t page);
+    static word_t getFrameByPage(word_t page);
 
     word_t findMaximalSeenFrame(word_t frame, unsigned int depth);
 
-    word_t findMaximalCyclicalDistance(word_t frame, unsigned int depth, const word_t ancestors[TABLES_DEPTH], word_t pageSwappedIn, word_t p, word_t *bestP);
+    word_t findMaximalCyclicalDistance(word_t frame, unsigned int depth, const word_t parents[TABLES_DEPTH],
+                                       word_t pageSwappedIn, word_t currP, word_t *bestP);
 
-    word_t readFrame(word_t addr, uint64_t offset);
+    static word_t readFrame(word_t addr, uint64_t offset);
 
-    void writeFrame(word_t addr, uint64_t offset, word_t value);
+    static void writeFrame(word_t addr, uint64_t offset, word_t value);
 
-    void traverseTree(uint64_t virtualAddress, int depth, word_t &addr, word_t ancestors[TABLES_DEPTH]);
+    void treeTraveling(uint64_t virtualAddress, int depth, word_t &addr, word_t parents[4]);
 
-    word_t handlePageFault(word_t prevAddr, uint64_t innerOffset, word_t current_page, int depth, word_t ancestors[TABLES_DEPTH]);
+    word_t handlePageFault(word_t prevAddr, uint64_t innerOffset, word_t current_page, int depth,
+                           word_t ancestors[TABLES_DEPTH]);
 };
 
 #endif // VIRTUALMEMORYMANAGER_H
