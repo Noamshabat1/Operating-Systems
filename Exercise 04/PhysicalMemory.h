0e110a19425c330aa4ec35425a7d3e960f5ba48c
Noam Shabat <92057540+Noamshabat1@users.noreply.github.com>
2024-07-17 22:45:27 +0300

    Update PhysicalMemory.h

--- Exercise 04/PhysicalMemory.h
+++ Exercise 04/PhysicalMemory.h
@@ -3,23 +3,30 @@
 #include "MemoryConstants.h"
 
 /*
- * Reads an integer from the given physical address and puts it in 'value'.
+ * reads an integer from the given physical address and puts it in 'value'
  */
 void PMread(uint64_t physicalAddress, word_t* value);
 
 /*
- * Writes 'value' to the given physical address.
+ * writes 'value' to the given physical address
  */
 void PMwrite(uint64_t physicalAddress, word_t value);
 
 
 /*
- * Evicts a page from the RAM to the hard drive.
+ * evicts a page from the RAM to the hard drive
  */
 void PMevict(uint64_t frameIndex, uint64_t evictedPageIndex);
 
 
 /*
- * Restores a page from the hard drive to the RAM.
+ * restores a page from the hard drive to the RAM
  */
 void PMrestore(uint64_t frameIndex, uint64_t restoredPageIndex);
+
+/*
+ * print the current state of the ram.
+ */
+void printRam();
+
+void printEvictionCounter();
